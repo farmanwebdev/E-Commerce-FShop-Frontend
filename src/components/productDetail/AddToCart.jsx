363acde2429150e9ce2a60e849eaf3b35048b053
farmanwebdev <saqibfarman633@gmail.com>
2026-02-06 14:27:59 +0500

    fix bugs

--- src/components/productDetail/AddToCart.jsx
+++ src/components/productDetail/AddToCart.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaPlus, FaMinus } from 'react-icons/fa';
 import { cartActions } from '../../store/cart-slice';
 import { useDispatch, useSelector } from 'react-redux';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import { motion } from 'framer-motion';
-
 import Hero from "../components/home/Hero";
 import Overview from '../components/home/Overview';
 import FeaturedProducts from '../components/home/FeaturedProducts';
